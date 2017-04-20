f5bfa6e19040b651ce1fb14b6a52cb961a44a351
Ruby Lim <ruby.b.lim@gmail.com>
2017-04-19 19:12:29 -0700

    checkpoint 6 send messages complete

--- app/scripts/controllers/RoomCtrl.js
+++ app/scripts/controllers/RoomCtrl.js
@@ -1,13 +1,21 @@
 (function () {
-    function RoomCtrl(Room, Message) {
+    function RoomCtrl(Room, Message, $cookies) {
         this.roomData = Room.all;
+        this.currentUser = $cookies.get('blocChatCurrentUser');
         this.setCurrentRoom = function(room) {
             this.currentRoom = room;
             this.messages = Message.getByRoomId(this.currentRoom.$id);
         };
+        
+        this.sendMessage = function () {
+            this.newMessage.roomId = this.currentRoom.$id;
+            this.newMessage.username = this.currentUser;
+            this.newMessage.sentAt = new Date().toString();
+            Message.send(this.newMessage);
+        }
     }
     
     angular
         .module('blocChat')
-        .controller('RoomCtrl', ['Room', 'Message', RoomCtrl]);
+        .controller('RoomCtrl', ['Room', 'Message', '$cookies', RoomCtrl]);
 })();
--- app/scripts/services/Message.js
+++ app/scripts/services/Message.js
@@ -1,16 +1,19 @@
 (function() {
-    function Message($firebaseArray) {
+    function Message($firebaseArray, $cookies) {
         var ref = firebase.database().ref().child("messages");
         var messages = $firebaseArray(ref);
         
         return {
             getByRoomId: function (roomId) { 
                 return $firebaseArray(ref.orderByChild("roomId").equalTo(roomId));
+            },
+            send: function (newMessage) {
+                messages.$add(newMessage);
             }
         };
     }
     
     angular
         .module('blocChat')
-        .factory('Message', ['$firebaseArray', Message]);
+        .factory('Message', ['$firebaseArray', '$cookies', Message]);
 })();
